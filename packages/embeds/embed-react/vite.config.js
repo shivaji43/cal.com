0fdbcd1061fe8b6c9bd6ac801962749f5649002d
Somay Chauhan <somaychauhan98@gmail.com>
2025-01-30 12:22:51 +0530

    Update vite.config.js

--- packages/embeds/embed-react/vite.config.js
+++ packages/embeds/embed-react/vite.config.js
@@ -1,32 +1,40 @@
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
+import EnvironmentPlugin from "vite-plugin-environment";
 
 import viteBaseConfig from "../vite.config";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  ...viteBaseConfig,
-  plugins: [react()],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.ts"),
-      name: "Cal",
-      fileName: (format) => `Cal.${format}.js`,
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), ""); // .env inside of packages/platform/atoms
+  return {
+    ...viteBaseConfig,
+    plugins: [react(), EnvironmentPlugin([])],
+    define: {
+      "process.env": env,
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: ["react", "react/jsx-runtime", "react-dom", "react-dom/client"],
-      output: {
-        exports: "named",
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, "src/index.ts"),
+        name: "Cal",
+        fileName: (format) => `Cal.${format}.js`,
+      },
+      rollupOptions: {
+        // make sure to externalize deps that shouldn't be bundled
+        // into your library
+        external: ["react", "react/jsx-runtime", "react-dom", "react-dom/client"],
+        output: {
+          exports: "named",
+          // Provide global variables to use in the UMD build
+          // for externalized deps
+          globals: {
+            react: "React",
+            "react-dom": "ReactDOM",
+          },
         },
       },
     },
-  },
+  };
 });
