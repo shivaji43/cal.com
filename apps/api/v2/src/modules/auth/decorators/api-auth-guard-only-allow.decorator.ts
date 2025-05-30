b4aa79da7da7d0deab7aed1a1d881905cfd58471
Shivaji Raut <108468912+shivaji43@users.noreply.github.com>
2025-05-30 19:33:10 +0530

    Delete apps/api/v2/src directory

--- apps/api/v2/src/modules/auth/decorators/api-auth-guard-only-allow.decorator.ts
+++
@@ -1,5 +0,0 @@
-import { Reflector } from "@nestjs/core";
-
-export type AllowedAuthMethod = "OAUTH_CLIENT_CREDENTIALS" | "API_KEY" | "ACCESS_TOKEN" | "NEXT_AUTH";
-
-export const ApiAuthGuardOnlyAllow = Reflector.createDecorator<AllowedAuthMethod[]>();
